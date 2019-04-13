1501a389a9a29ea3fa2d3d2778b353e2b1a0b8a6
nero <simon.iavarone@student.uclouvain.be>
2019-04-13 13:25:22 +0200

    creation de AppContextProvider qui permet d'obtenir le contexte courant et création des constructeurs de User

--- app/src/main/java/com/example/iqwhizz/User.java
+++ app/src/main/java/com/example/iqwhizz/User.java
@@ -1,27 +1,16 @@
 package com.example.iqwhizz;
 
-import android.arch.persistence.room.ColumnInfo;
-import android.arch.persistence.room.Entity;
-import android.arch.persistence.room.PrimaryKey;
-import android.support.annotation.NonNull;
-
-
 import java.util.Date;
-
 
 public class User {
 
     public String username;
-
     public String password;
-
     public String language;
-
     public String mail;
-
     public int birthdate;
-
     public String picture;
+    public int registrationDate;
 
     /*
     /  Crée un Usser sur base de toutes ses données,
@@ -35,10 +24,10 @@ public class User {
         birthdate = bd;
         picture = pic;
         // donne l'heure d'inscription (a vérifier)
-        int insc_d = (int) new Date().getTime();
+        registrationDate = (int) new Date().getTime();
         // a changer si implémenté
         int last_co = 0;
-        UserDAO.createUser(u,p,l,m,bd,insc_d,last_co,pic,AppContextProvider.getContext());
+        UserDAO.createUser(u,p,l,m,bd,registrationDate,last_co,pic,AppContextProvider.getContext());
     }
     public User(String username, String password, String language) {
         if(UserDAO.getUser(username,password,AppContextProvider.getContext()) != null ) {
